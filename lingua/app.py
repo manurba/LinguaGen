2839be0089a982ecccfd7176d0ad513115c2bf35
manurba <manuel50urbano@gmail.com>
2024-02-25 02:45:46 +0000

    Adding MongoDB conversation logging

--- lingua/app.py
+++ lingua/app.py
@@ -2,17 +2,53 @@ import io
 import os
 import uuid
 
+import aiofiles
+import motor.motor_asyncio
+from dotenv import load_dotenv
 from fastapi import FastAPI, File, Form, UploadFile
 
 from lingua.agents.LinguaAgent import LinguaGen
 from lingua.utils.dataclass import audio2text, text2audio
 
+load_dotenv()
+
+# Initialize MongoDB client and select your database and collection
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URI"))
+db = client.conversations_database
+conversations_collection = db.get_collection(os.getenv("MONGO_DB_COLLECTION"))
+
+# Initialize FastAPI app
 app = FastAPI()
 
 
 @app.get("/new_conversation")
 async def new_conversation():
-    return {"conversation_id": str(uuid.uuid4())}
+    conversation_id = uuid.uuid4().hex
+    # Create a new conversation entry in MongoDB with a system message
+    await conversations_collection.insert_one(
+        {
+            "_id": conversation_id,
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."}
+            ],
+        }
+    )
+    return {"conversation_id": conversation_id}
+
+
+async def update_or_create_conversation(conversation_id, new_message):
+    # Check if the conversation already exists
+    conversation = await conversations_collection.find_one(
+        {"_id": conversation_id}
+    )
+    print(conversation)
+
+    if conversation:
+        # If it exists, append the new message
+        await conversations_collection.update_one(
+            {"_id": conversation_id},
+            {"$push": {"messages": {"$each": new_message}}},
+        )
 
 
 @app.post("/get_response")
@@ -21,6 +57,12 @@ async def compute_reply(
 ):
     audio_file = io.BytesIO(await file.read())
 
+    get_conversation = await conversations_collection.find_one(
+        {"_id": conversation_id}
+    )
+
+    conversation = get_conversation["messages"]
+
     response = await audio2text(
         request_url="https://api.openai.com/v1/audio/transcriptions",
         request_header={"Authorization": f"Bearer {os.getenv('API_KEY')}"},
@@ -28,35 +70,20 @@ async def compute_reply(
         model="whisper-1",
     )
 
-    request_audio = {
-        "parent_message_id": conversation_id,
-        "messages": [
-            {
-                "id": uuid.uuid4(),
-                "author": {"role": "user"},
-                "content": {
-                    "content_type": "text",
-                    "parts": [response["text"]],
-                },
-            }
-        ],
-    }
+    # Extract the text part from the response
+    text_response = response["text"]
 
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": request_audio["messages"][0]["content"]["parts"][0],
-        },
-    ]
+    conversation.append({"role": "user", "content": text_response})
+
+    id_request_audio = uuid.uuid4().hex
 
     lingua = LinguaGen()
 
     response = await lingua.request_handler(
-        request_id=request_audio["parent_message_id"],
+        request_id=id_request_audio,
         request_json={
             "model": "gpt-3.5-turbo-0125",
-            "messages": messages,
+            "messages": conversation,
             "max_tokens": 100,
         },
         request_url="https://api.openai.com/v1/chat/completions",
@@ -66,7 +93,9 @@ async def compute_reply(
         max_attempts=5,
     )
 
-    response_text = response[conversation_id]["response"]
+    lingua_response = response[conversation_id]["response"]
+
+    conversation.append({"role": "assistant", "content": lingua_response})
 
     response = await text2audio(
         request_url="https://api.openai.com/v1/audio/speech",
@@ -75,12 +104,14 @@ async def compute_reply(
             "Content-Type": "application/json",
         },
         voice="alloy",
-        input=response_text,
+        input=lingua_response,
         model="tts-1",
     )
 
     file_name = f"data/{conversation_id}_output.mp3"
-    with open(file_name, "wb") as audio_file:
-        audio_file.write(response)
+    async with aiofiles.open(file_name, "wb") as audio_file:
+        await audio_file.write(response)
 
-    return {"file": file_name, "response_text": response_text}
+    await update_or_create_conversation(conversation_id, conversation)
+
+    return {"file": file_name, "conversation": conversation}
